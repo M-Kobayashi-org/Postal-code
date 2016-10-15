71858bd4a8b23f50daff07a86a7138550993ba05
Masato Kobayashi <m.kobayashi.org@gmail.com>
2016-10-15 22:42:01 +0900

    ストリームフィルタを使って、UTF-8に変換

--- Source/import.php
+++ Source/import.php
@@ -1,4 +1,7 @@
 <?php
+mb_language('Japanese');
+mb_internal_encoding('UTF-8');
+
 $zip_file = 'ken_all_rome.zip';
 $zip_url = 'http://www.post.japanpost.jp/zipcode/dl/roman/'.$zip_file;
 $tmp_dir = sys_get_temp_dir();
@@ -28,8 +31,11 @@ try {
 				$entry = $cmp->getNameIndex($i);
 				if ( substr( $entry, -1 ) == '/' ) continue;
 				$fp = $cmp->getStream( $entry );
-				$ofp = fopen( $tmp_dir.'/'.$entry, 'wb' );
 				if (!$fp) throw new Exception('Unable to extract the file.');
+				if (!stream_filter_prepend($fp, 'convert.iconv.cp932/utf-8', STREAM_FILTER_READ))
+					throw new Exception('Counld not apply stream filter.');
+				$ofp = fopen( $tmp_dir.'/'.$entry, 'wb' );
+				if (!$ofp) throw new Exception('File can not be opened. :'.$entry);
 
 				while (!feof($fp))
 					fwrite($ofp, fread($fp, 8192));
